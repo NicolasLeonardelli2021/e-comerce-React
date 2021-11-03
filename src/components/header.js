ebd19c2c97afedc0029877e6b2b510a5ca999263
Nicolas Leonardelli <nicolasleonardelli2012@gmail.com>
2021-11-03 20:36:18 -0300

    header

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export const Header = () => {
+  
+return (
+<div className = "header">
+    <div className="barraSuperior">
+        <a className="inicio" href="">Iniciar Sesion</a>
+    </div>
+    <div className="barraLogo">
+        
+    </div>
+
+</div>
+)
+}
